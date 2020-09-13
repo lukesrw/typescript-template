e3e5a9ef518c5eb9a816ec769cabd6c0c805ced3
lukesrw <lukesrw+github@gmail.com>
2020-09-13 16:05:45 +0100

    Updated watch script

--- watch.js
+++ watch.js
@@ -5,19 +5,20 @@ const TARGET = __dirname;
 
 let queue = {};
 let events = {
-    "change .md$": "remark -o",
     "change .scss$": "sass dist/src/public/css",
     "change ^src.+.ts$": "tsc"
 };
 
-exec("npm run watch-start --if-present", (error, stdout, stderr) => {
+exec(`chdir /D ${__dirname} && npm run watch-start --if-present`, (error, stdout, stderr) => {
     if (error || stderr) {
         console.log(`${error || stderr} @ ${new Date()}`);
     }
 
     console.log(`Watching ${TARGET} @ ${new Date()}`);
 
-    exec("npm run watch-after-start --if-present");
+    exec(`chdir /D ${__dirname} && npm run watch-after-start --if-present`).stdout.on("data", chunk => {
+        console.log(chunk.trim());
+    });
 
     watch(
         TARGET,
@@ -42,7 +43,7 @@ exec("npm run watch-start --if-present", (error, stdout, stderr) => {
 
                 console.log(`Executing '${events[pattern]}' @ ${new Date()}`);
 
-                exec(events[pattern], (error, stdout, stderr) => {
+                exec(`chdir /D ${__dirname} && ${events[pattern]}`, (error, stdout, stderr) => {
                     if (error || stderr) {
                         console.log(`${error || stderr} @ ${new Date()}`);
                     } else {
@@ -60,7 +61,7 @@ exec("npm run watch-start --if-present", (error, stdout, stderr) => {
 
 process.on("SIGINT", () => {
     try {
-        execSync("npm run watch-end --if-present");
+        execSync(`chdir /D ${__dirname} && npm run watch-end --if-present`);
     } catch (ignore) {}
 
     process.exit(); // eslint-disable-line
