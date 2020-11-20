a2ac22b54160cdc482a4f75544996c05af5d5b11
lukesrw <lukesrw+github@gmail.com>
2020-11-20 20:55:58 +0000

    Improved to remove directories

--- clean.js
+++ clean.js
@@ -28,16 +28,12 @@ function isCompiledFile(file, files) {
     /**
      * SCSS filter
      */
-    if (
+    return (
         (file.endsWith(".css") &&
             (files.includes(`${file_no_suffix}.scss`) || files.includes(`_${file_no_suffix}.scss`))) ||
         (file.endsWith(".css.map") &&
             (files.includes(`${file_no_suffix_again}.scss`) || files.includes(`_${file_no_suffix_again}.scss`)))
-    ) {
-        return true;
-    }
-
-    return false;
+    );
 }
 
 /**
@@ -45,18 +41,32 @@ function isCompiledFile(file, files) {
  * @returns {void}
  */
 async function clean(source = []) {
-    let clean_items = await fs.promises.readdir(path.join(...source));
+    try {
+        let clean_items = await fs.promises.readdir(path.join(...source));
+        let clean_items_unlinked = 0;
 
-    for (let index = 0; index < clean_items.length; index += 1) {
-        let clean_item_path = path.join(...source.concat(clean_items[index]));
-        let clean_item_stats = await fs.promises.stat(clean_item_path);
-        if (clean_item_stats.isDirectory()) {
-            await clean(source.concat(clean_items[index]));
+        for (let index = 0; index < clean_items.length; index += 1) {
+            let clean_item_path = path.join(...source.concat(clean_items[index]));
+            let clean_item_stats = await fs.promises.stat(clean_item_path);
+            if (clean_item_stats.isDirectory()) {
+                if (await clean(source.concat(clean_items[index]))) {
+                    clean_items_unlinked += 1;
+                }
+            }
+            if (clean_item_stats.isFile() && isCompiledFile(clean_items[index], clean_items)) {
+                await fs.promises.unlink(clean_item_path);
+                clean_items_unlinked += 1;
+            }
         }
-        if (clean_item_stats.isFile() && isCompiledFile(clean_items[index], clean_items)) {
-            await fs.promises.unlink(clean_item_path);
+
+        if (clean_items_unlinked === clean_items.length) {
+            await fs.promises.rmdir(path.join(...source));
+
+            return true;
         }
-    }
+    } catch (_1) {}
+
+    return false;
 }
 
-clean([__dirname, "dist", "src"]);
+clean([__dirname, "dist"]);
