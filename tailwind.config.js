93a4cb935f4b5cc46ec5359f141d44b8caa13580
lukesrw <lukesrw+github@gmail.com>
2021-01-05 22:40:48 +0000

    Updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     darkMode: "media",
     plugins: [],
-    purge: ["./dist/**/*.html", "./dist/**/*.js"],
+    purge: ["./**/*.html", "./**/*.js"],
     theme: {
         extend: {}
     },
